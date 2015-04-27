6287194a6f1022b8035855c9a937c1683b83ea72
Fredrik Näslund <c99fnd@users.noreply.github.com>
2015-04-28 00:40:01 +0200

    Fixes to PhotoService interface

--- app/src/main/java/com/fredde/flickrsearch/data/PhotosService.java
+++ app/src/main/java/com/fredde/flickrsearch/data/PhotosService.java
@@ -1,5 +1,9 @@
 package com.fredde.flickrsearch.data;
 
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Query;
+
 /**
  * Retrofit Service Interface
  */
@@ -8,5 +12,5 @@ public interface PhotosService {
     @GET("/rest/")
     void getPhotos(@Query("method") String method, @Query("api_key") String apiKey, @Query("tags")
     String tags, @Query("per_page") String perPage, @Query("format") String format,
-                   Callback<com.fredde.flickrsearch.data.PhotoItem> data);
+                   Callback<PhotoItem> data);
 }
